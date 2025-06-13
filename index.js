1b77bc9c0221999c5a4ece8bc95ade9994f438b3
Marshall Cyrille Marcelo <mcam49856@gmail.com>
2025-06-13 23:45:50 +0800

    completed js for switching time

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ const filter_list = document.getElementById("filter-list");
 const cards = document.querySelectorAll(".card");
 const [profile, ...metrics] = cards;
 const time_cards = [...metrics];
-console.log(time_cards);
 
 fetchData().then((data) => {
-  console.log(data);
   filter_list.addEventListener("click", (e) => {
     let duration = e.target.getAttribute("value");
-    for (let [i, value] of data.entries()) {
-      console.log(`${i}`);
-    }
+
+    time_cards.forEach((card, i) => {
+      const title = card.querySelector(".term");
+      const total = card.querySelector(".total");
+      const last = card.querySelector(".last");
+      const timeframe = data[i].timeframes[duration];
+      const previous = data[i].timeframes[duration].previous;
+
+      title.textContent = data[i].title;
+      total.textContent = `${timeframe.current}hrs`;
+      let label;
+      switch (duration) {
+        case "daily":
+          label = "Yesterday - ";
+          break;
+        case "weekly":
+          label = "Last Week - ";
+          break;
+        case "monthly":
+          label = "Last Month - ";
+          break;
+      }
+      last.textContent = `${label}${previous}`;
+    });
   });
 });
